3a6135d7fa0c3f8cdc44702ba0dfc89d23213214
kvswim <kvswim@gmail.com>
2016-09-25 18:36:34 -0400

    assignment 2 and 3

---
+++ Assignment 2/Assignment 2/LengthException.java
@@ -0,0 +1,13 @@
+/*
+ * Kyle Verdeyen
+ * kverdey1@jhu.edu
+ */
+/**
+    Exception for invalid length.
+
+    Data structures that have a fixed (integer) length throw
+    LengthException if a given length is out of range.
+*/
+public class LengthException extends RuntimeException {
+    private static final long serialVersionUID = 0L;
+}
---
+++ Assignment 3/build.sh
@@ -0,0 +1,61 @@
+sudo javac -cp "/usr/share/java/junit4-4.12.jar" -Xlint:all Array.java GnomeSort.java IndexException.java NullSort.java PolySort.java SelectionSort.java SimpleArray.java SortingAlgorithm.java Statable.java StatableArray.java StatableArrayTest.java BubbleSort.java InsertionSort.java
+printf "Random data: \n"
+java PolySort 1000 <random.data
+printf "\n"
+java PolySort 2000 <random.data
+printf "\n"
+java PolySort 3000 <random.data
+printf "\n"
+java PolySort 4000 <random.data
+printf "\n"
+java PolySort 5000 <random.data
+printf "\n"
+java PolySort 6000 <random.data
+printf "\n"
+java PolySort 7000 <random.data
+printf "\n"
+java PolySort 8000 <random.data
+printf "\n"
+java PolySort 9000 <random.data
+printf "\n"
+java PolySort 10000 <random.data
+printf "\nAscending Data:\n"
+java PolySort 1000 <ascending.data
+printf "\n"
+java PolySort 2000 <ascending.data
+printf "\n"
+java PolySort 3000 <ascending.data
+printf "\n"
+java PolySort 4000 <ascending.data
+printf "\n"
+java PolySort 5000 <ascending.data
+printf "\n"
+java PolySort 6000 <ascending.data
+printf "\n"
+java PolySort 7000 <ascending.data
+printf "\n"
+java PolySort 8000 <ascending.data
+printf "\n"
+java PolySort 9000 <ascending.data
+printf "\n"
+java PolySort 10000 <ascending.data
+printf "\nDescending data:\n"
+java PolySort 1000 <descending.data
+printf "\n"
+java PolySort 2000 <descending.data
+printf "\n"
+java PolySort 3000 <descending.data
+printf "\n"
+java PolySort 4000 <descending.data
+printf "\n"
+java PolySort 5000 <descending.data
+printf "\n"
+java PolySort 6000 <descending.data
+printf "\n"
+java PolySort 7000 <descending.data
+printf "\n"
+java PolySort 8000 <descending.data
+printf "\n"
+java PolySort 9000 <descending.data
+printf "\n"
+java PolySort 10000 <descending.data
